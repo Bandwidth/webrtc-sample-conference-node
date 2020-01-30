e7dd32965639653af778b43703a627a1691b1fbc
Jeff Turner <jturner@bandwidth.com>
2020-01-30 13:24:21 -0500

    Fix for missing index.html on hard refresh

--- server.ts
+++ server.ts
@@ -405,7 +405,7 @@ const publishStreamToAllParticipants = (
 app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
 });
 
 app.listen(port, async () => {
